e20f7e42da7c0d28c3859b0be195d8ebca1fac39
Eyal Farago <eyal.farago@actimize.com>
2015-11-26 16:06:06 +0200

    mb3-109-reopen: good and bad tests.

--- mybatis-scala-core/src/test/scala/org/mybatis/scala/mapping/MBIssue39ReproSpec.scala
+++ mybatis-scala-core/src/test/scala/org/mybatis/scala/mapping/MBIssue39ReproSpec.scala
@@ -1,5 +1,6 @@
 package org.mybatis.scala.mapping
 
+import org.mybatis.scala.mapping.MBIssue39ReproSpec.{UserProps, UserId}
 import org.mybatis.scala.{Database, DatabaseSupport}
 import org.mybatis.scala.domain.User
 import org.mybatis.scala.infrastructure.UserRepository
@@ -10,6 +11,12 @@ import org.scalatest.{ShouldMatchers, FunSuite}
   * Created by eyal on 26/11/2015.
   */
 class MBIssue39ReproSpec extends FunSuite with ShouldMatchers with DatabaseSupport{
+
+  Database.config.addSpace("MBIssue39ReproSpec"){ space =>
+    space ++= MBIssue39ReproSpec.Bad
+    space ++= MBIssue39ReproSpec.Good
+  }
+
   val users = for( i <- 1 to 10 ) yield{
     User( i, s"user$i", s"user$i@my.place" )
   }
@@ -31,10 +38,84 @@ class MBIssue39ReproSpec extends FunSuite with ShouldMatchers with DatabaseSuppo
     }
   }
 
+  test( "Bad" ){
+    import MBIssue39ReproSpec.Bad
+    withUsers{ implicit session =>
+      val badUsers = Bad.selectUsers()
+      badUsers should have size ( users.size )
+    }
+  }
+
+  test( "Good" ){
+    import MBIssue39ReproSpec.Good
+    withUsers{ implicit session =>
+      val goodUsers = Good.selectUsers()
+      goodUsers should have size ( users.size )
+
+      val us = for( gu <- goodUsers ) yield{
+        val uid = gu.userId
+        val userProps = Option( gu.userProps ) getOrElse  new MBIssue39ReproSpec.UserProps
+        val user = new User( uid, userProps.userName, userProps.email )
+        user
+      }
+      us.toSet shouldEqual users.toSet
+
+    }
+  }
+
 }
 
 object MBIssue39ReproSpec{
 
-  object Bad
+  class UserId{
+    var userId : Int = _
+  }
+  class UserProps{
+    var userName : String = _
+    var email : String = _
+  }
 
+  object UserIdMapping extends ResultMap[UserId]{
+    result( "userId", "id", T[Int])
+  }
+  object UserPropsMapping extends ResultMap[UserProps]{
+    result( "userName", "name", T[String])
+    result( "email", "email", T[String])
+  }
+
+  object Bad{
+
+    class User{
+      var userId : UserId = _
+      var userProps : UserProps = _
+    }
+
+    object UserMapping extends ResultMap[User]{
+      association("userId",resultMap = UserIdMapping )
+      association("userProps", resultMap = UserPropsMapping)
+    }
+
+    val selectUsers = new SelectList[User] {
+      resultMap = UserMapping
+      override def xsql: XSQL = <xsql>SELECT * FROM user</xsql>
+    }
+
+    def bind = Seq( selectUsers )
+  }
+
+  object Good{
+    class User extends UserId{
+      var userProps : UserProps = _
+    }
+    object UserMapping extends ResultMap[User]( UserIdMapping ){
+      association("userProps", resultMap = UserPropsMapping)
+    }
+
+    val selectUsers = new SelectList[User] {
+      resultMap = UserMapping
+      override def xsql: XSQL = <xsql>SELECT * FROM user</xsql>
+    }
+
+    def bind = Seq( selectUsers )
+  }
 }
